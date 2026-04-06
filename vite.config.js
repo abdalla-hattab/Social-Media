c397e4a85fb6f01ad9beea0cc1ed972e8a0a0157
Zeiad Hattab <3tooralfaisal@gmail.com>
2026-04-06 05:35:44 +0300

    Update for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 export default {
+  base: '/Socail-Media/',
   server: {
     host: '0.0.0.0', // Listen on all networks so localhost works
     allowedHosts: true, // Allow all hosts (like Localtunnel)
